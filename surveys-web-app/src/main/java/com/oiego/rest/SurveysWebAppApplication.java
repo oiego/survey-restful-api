5c1dd7dc0f5221568fcc2746fda43981ea8aa580
oiego <diegomenesest@gmail.com>
2020-10-09 22:02:56 -0500

    first commit uploading all files

---
+++ surveys-web-app/src/main/java/com/oiego/rest/SurveysWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.oiego.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveysWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveysWebAppApplication.class, args);
+	}
+
+}
